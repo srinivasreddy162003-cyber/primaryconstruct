55bd8376ec9cdec4c93f29d9b4580c923570930e
Nikhil Razz <nikhilhash03@gmail.com>
2026-05-25 15:13:04 +0530

    inhertiance

--- primaryconstruct/Program.cs
+++ primaryconstruct/Program.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace primaryconstruct
-{
-    class myclass
-    {
-         internal int a = 10;
-        public void mymethod()
-        {
-            Console.WriteLine("value under mymethos is " + a);
-        }
-    }
-    class newclass
-    {
-        public void newmethod()
-        {
-           myclass obj = new myclass();
-            Console.WriteLine("value under newmethod is " + obj.a);
-        }
-    }
+//namespace primaryconstruct
+//{
+//    class myclass
+//    {
+//         internal int a = 10;
+//        public void mymethod()
+//        {
+//            Console.WriteLine("value under mymethos is " + a);
+//        }
+//    }
+//    class newclass
+//    {
+//        public void newmethod()
+//        {
+//           myclass obj = new myclass();
+//            Console.WriteLine("value under newmethod is " + obj.a);
+//        }
+//    }
         
 
    
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            myclass mc = new myclass();
-            mc.mymethod();
-            Console.WriteLine("value of a under main method " + mc.a);
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass mc = new myclass();
+//            mc.mymethod();
+//            Console.WriteLine("value of a under main method " + mc.a);
 
-            newclass nc = new newclass();
-            nc.newmethod();
-        }
-    }
-}
+//            newclass nc = new newclass();
+//            nc.newmethod();
+//        }
+//    }
+//}
---
+++ primaryconstruct/Properties/inheritance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primaryconstruct.Properties
+{
+    class branch
+    {
+        int bid;
+        string branchname;
+        string branchaddress;
+        public branch(int bid, string branchname,string branchaddress)
+        {
+            this.bid = bid;
+            this.branchname = branchname;
+            this.branchaddress = branchaddress;
+        }
+        public void branchdisplay()
+        {
+            Console.WriteLine("Branch id is " + bid);
+            Console.WriteLine("Branch name is " + branchname);
+            Console.WriteLine("Branch address is " + branchaddress);
+        }
+    }
+    class student:branch
+    {
+        int sid;
+        string sname;
+        String scollege;
+        public student(int sid, string sname, string scollege):base(111,"AAA","KPHB")
+        {
+            this.sid = sid;
+            this.sname = sname;
+            this.scollege = scollege;
+            base.branchdisplay();
+        }
+        public void studentdisplay()
+        {
+            Console.WriteLine("Student id is" + sid);
+            Console.WriteLine("Student name is " + sname);
+            Console.WriteLine("Student college is "+scollege);
+        }
+    }
+    internal class inheritance
+    {
+        static void Main(string[] args)
+        {
+            student obj = new student(1, "Nikhil", "VIT");
+            obj.studentdisplay();
+        }
+    }
+}
